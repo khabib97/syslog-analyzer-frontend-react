32a8922e32a1e4d8cf550a4d155598f560881927
Kawser Habib <kawser.habib.dev@gmail.com>
2021-05-06 14:39:36 +0600

    add navlink and route for histogram and syslog table

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Container } from "reactstrap";
 import "./index.css";
 import SyslogTable from "./SyslogTable";
 import { Route, NavLink, HashRouter } from "react-router-dom";
-import Histogram from "./Histogram";
+import SyslogHistogram from "./SyslogHistogram";
 
 const App = () => {
   
@@ -15,16 +15,16 @@ const App = () => {
           <h1>Sysloag Analyzer</h1>
           <ul className="header">
             <li>
-              <NavLink to="/" activeClassName="selected">Analyzer</NavLink>
+              <NavLink to="/">Analyzer</NavLink>
             </li>
             <li>
-              <NavLink to="/histogram" activeClassName="active">Histogram</NavLink>
+              <NavLink to="/histogram" >Histogram</NavLink>
             </li>
           </ul>
         </div>
         <div className="content">
             <Route exact path="/" component={SyslogTable}/>
-            <Route path="/histogram" component={Histogram}/>
+            <Route path="/histogram" component={SyslogHistogram}/>
         </div>
       </HashRouter>
     </Container>
